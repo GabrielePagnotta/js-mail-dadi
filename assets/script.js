4281d091eb7d35a157ac127b08a19a03f10e36c8
GabrielePagnottae <gabriele.pagnotta1998@gmail.com>
2022-10-24 17:28:41 +0200

    push 1

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,27 @@
-let = 
+let inputemail = document.getElementById('mail');
+
+
+function checkemail(){
+    let emailvalue = inputemail.value;
+    let emaillist = ["peppino@gmail.com", "patrizio@gmail.com", "armando@gmail.com"]
+
+
+
+    for (let i = 0; i < emaillist.length; i++){
+    
+    if (emailvalue == emaillist[i]){
+        console.log(`${emailvalue} "è presente nel database alla sezione: "${i}`)
+    }
+
+    else if(emailvalue != emaillist[i]){
+        console.log(`${emailvalue} "non è presente nel database"`)
+    }
+}
+
+
+}
+
+
+
+
+
