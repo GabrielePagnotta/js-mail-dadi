6f69b7141e86fe4ac48a17790a1ac155c397c267
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2022-10-25 01:55:55 +0200

    push 3

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,66 @@
-let inputemail = document.getElementById('mail');
+// let inputemail = document.getElementById('mail');
 
 
-function checkemail(){
-    let emailvalue = inputemail.value;
-    let emaillist = ["peppino@gmail.com", "patrizio@gmail.com", "armando@gmail.com"]
+// function checkemail(){
+//     let emailvalue = inputemail.value;
+//     let emaillist = ["peppino@gmail.com", "patrizio@gmail.com", "armando@gmail.com"]
 
 
-    let emailtf = false;
-    for (let i = 0; i < emaillist.length; i++){
+//     let emailtf = false;
+//     for (let i = 0; i < emaillist.length; i++){
     
-    if (emailvalue == emaillist[i]){
-       emailtf = true;
-}
+//     if (emailvalue == emaillist[i]){
+//        emailtf = true;
+// }
+
+// }
+
+// if (emailtf == true){
+//     console.log(`${emailvalue} "è corretta"`);
+// }
+
+// else {
+//     console.log(`${emailvalue} "è sbagliata"`);
+// }
+
+// }
+
+// esercizio 2
+
+let playerWin = 0;
+let pcWin = 0;
+
+let winPlayer = document.getElementById("playerCount");
+let winPc = document.getElementById("pcCount");
+
+function startInput() {
+
+    // VARIABILI            
+
+    let result = document.getElementById("resultOut");
+    let playerNumber = Math.floor(Math.random() * 6 + 1);
+    let pcNumber = Math.floor(Math.random() * 6 + 1);
+
+    console.log(playerNumber);
+    console.log(pcNumber);
+
+    //Condizioni
+
+    if (playerNumber > pcNumber) {
+        result.innerHTML = `Hai vinto! il tuo numero è ${playerNumber}`;
+        playerWin = playerWin + 1;
+
+    } else if (playerNumber == pcNumber) {
+        result.innerHTML = `Pareggio! ${playerNumber} e ${pcNumber}! `;
+    } else {
+        result.innerHTML = `Hai perso D: ho fatto ${pcNumber}, Riproviamo?`;
+        pcWin = pcWin + 1;
+    }
+
+    winPlayer.innerHTML = `Vittorie: ${playerWin}`;
+    winPc.innerHTML = `Perdite: ${pcWin}`;
 
 }
-
-if (emailtf == true){
-    console.log(`${emailvalue} "è corretta"`);
-}
-
-else {
-    console.log(`${emailvalue} "è sbagliata"`);
-}
-
-}
-
 
 
 
